df96073a80b523c605d19cea66547aac21cffe0c
Jonatas Caetano <jonatas.calves@gmail.com>
2023-09-10 19:20:23 -0300

    refactor: added category to product

--- lib/layers/data/utils/data_local_mock.dart
+++ lib/layers/data/utils/data_local_mock.dart
@@ -2,32 +2,58 @@ import 'package:corelab_app_challenge/layers/data/models/category_model.dart';
 import 'package:corelab_app_challenge/layers/data/models/product_model.dart';
 
 class DataLocalMock {
+  static List<String> categories = [
+    'Anestésicos e Agulhas Gengival',
+    'Biossegurança',
+    'Descartáveis',
+    'Dentística e Estética',
+    'Ortodontia',
+    'Endodontia',
+    'Periféricos e Peças de Mão',
+    'Moldagem',
+    'Prótese',
+    'Cimentos',
+    'Instrumentos',
+    'Cirurgia e Periodontia',
+    'Radiologia',
+  ];
+
   static List<ProductModel> products = [
     ProductModel(
-        nameModel: 'Condicionador Ácido Fosfórico 37% - AllPrime',
-        imageUrlModel: "assets/img/allprime.jpg",
-        priceModel: 29.90,
-        discountModel: 0),
+      nameModel: 'Condicionador Ácido Fosfórico 37% - AllPrime',
+      imageUrlModel: "assets/img/allprime.jpg",
+      priceModel: 29.90,
+      discountModel: 0,
+      categoryModel: categories[3],
+    ),
     ProductModel(
-        nameModel: 'Dorflex Analgésico e Relaxante Muscular 36 comprimidos',
-        imageUrlModel: "assets/img/dorflex.jpg",
-        priceModel: 300.00,
-        discountModel: 10),
+      nameModel: 'Dorflex Analgésico e Relaxante Muscular 36 comprimidos',
+      imageUrlModel: "assets/img/dorflex.jpg",
+      priceModel: 300.00,
+      discountModel: 10,
+      categoryModel: categories[4],
+    ),
     ProductModel(
-        nameModel: 'Protetor Solar Corporal Cenoura & Bronze FPS 30 com 200ml',
-        imageUrlModel: "assets/img/protetor.jpg",
-        priceModel: 42.99,
-        discountModel: 10),
+      nameModel: 'Protetor Solar Corporal Cenoura & Bronze FPS 30 com 200ml',
+      imageUrlModel: "assets/img/protetor.jpg",
+      priceModel: 42.99,
+      discountModel: 10,
+      categoryModel: categories[0],
+    ),
     ProductModel(
-        nameModel: 'Shampoo Antiqueda Vichy Dercos Energizante 400ml',
-        imageUrlModel: "assets/img/energizante.jpg",
-        priceModel: 159.90,
-        discountModel: 0),
+      nameModel: 'Shampoo Antiqueda Vichy Dercos Energizante 400ml',
+      imageUrlModel: "assets/img/energizante.jpg",
+      priceModel: 159.90,
+      discountModel: 0,
+      categoryModel: categories[2],
+    ),
     ProductModel(
-        nameModel: 'Máscara Descartável KN95 Multilaser PFF2 HC124 1 Unidade',
-        imageUrlModel: "assets/img/mascara.jpg",
-        priceModel: 6.59,
-        discountModel: 0),
+      nameModel: 'Máscara Descartável KN95 Multilaser PFF2 HC124 1 Unidade',
+      imageUrlModel: "assets/img/mascara.jpg",
+      priceModel: 6.59,
+      discountModel: 0,
+      categoryModel: categories[2],
+    ),
   ];
 
   static List<ProductModel> productsYesterday = [
@@ -45,22 +71,6 @@ class DataLocalMock {
     'Anestésicos e Agulhas Gengival',
     'Biossegurança',
     'Descartáveis',
-  ];
-
-  static List<String> categories = [
-    'Anestésicos e Agulhas Gengival',
-    'Biossegurança',
-    'Descartáveis',
-    'Dentística e Estética',
-    'Ortodontia',
-    'Endodontia',
-    'Periféricos e Peças de Mão',
-    'Moldagem',
-    'Prótese',
-    'Cimentos',
-    'Instrumentos',
-    'Cirurgia e Periodontia',
-    'Radiologia',
   ];
 
   static List<CategoryModel> categoriesModel = [
--- lib/layers/domain/entities/product_entity.dart
+++ lib/layers/domain/entities/product_entity.dart
@@ -1,14 +1,17 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+
 class ProductEntity {
   String name;
   String imageUrl;
   double price;
   int discount;
+  String category;
 
   ProductEntity({
     required this.name,
     required this.imageUrl,
     required this.price,
     required this.discount,
+    required this.category,
   });
 }
